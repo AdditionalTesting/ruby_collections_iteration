33ced039c088844ff3fedbe184cb47a4ea891145
AlB <albert.burc@gmail.com>
2017-06-28 13:36:31 -0400

    Exercise0 complete

---
+++ exercises.rb
@@ -0,0 +1,12 @@
+#Exercise 0
+
+colors = ['red', 'blue', 'green']
+ages = [12, 18, 27, 40.5, 60.25, 90.125]
+coin_flip = ['heads','heads', 'heads', 'tails', 'tails' ]
+artists = ['Radiohead', 'Justin Bieber', 'KenM']
+colors = [:red, :blue, :green]
+
+words = {"CPU" => "Central Processing Unit", "RAM" => "Random Access Memory","LAN" => "Local Area Network" }
+movies = {"Burn After Reading" => 2008, "The Other Guys" => 2010, "Exit Through the Gift Shop" => 2010}
+cities = {"Toronto" => 2700000, "Madrid" => 3141991, "Rotterdam" => 1015215}
+names = {"JJ" => 28, "Ross" => 31, "Max" => 26}
